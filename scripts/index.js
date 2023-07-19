e62a906cd0f93823ea1e937200bd18d9d519faf2
hermtechs <hermtechs@gmail.com>
2023-07-19 18:54:31 +0300

    added a modal -image-viewer for clicked photos

--- scripts/index.js
+++ scripts/index.js
@@ -37,3 +37,27 @@ function showAllProducts() {
     });
   });
 }
+
+
+//image viewer/modal
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var productImg = document.querySelectorAll(".product-image");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+productImg.forEach(img=>img.addEventListener('click', (e)=>{
+    modal.style.display = "block";
+    // modalImg.src = this.src;
+    modalImg.src = e.currentTarget.src
+    captionText.innerHTML = this.alt;
+}))
+
+// Get the <span> element that closes the modal
+var span = document.querySelector(".close-photo-viewer");
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
+}
